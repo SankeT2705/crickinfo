8e463019fa38eb7a2d87044b70e572096d2a99a8
Sanket Raghunath Bobhate <76879335+SankeT2705@users.noreply.github.com>
2023-03-24 09:00:34 +0530

    first commit

---
+++ build/precache-manifest.39cf8f8967fd50577acadda430e47801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a61ef5a15e6fe451eda6",
+    "url": "/static/css/main.b2566814.chunk.css"
+  },
+  {
+    "revision": "a61ef5a15e6fe451eda6",
+    "url": "/static/js/main.a61ef5a1.chunk.js"
+  },
+  {
+    "revision": "069e117efe9d250d6662",
+    "url": "/static/js/1.069e117e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42e8ba9b598a60d111baaf7559c280f7",
+    "url": "/index.html"
+  }
+];
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbar from './component/navbar';
+import './style.css';
+
+import HomeSection from './component/homeSection';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+  <React.StrictMode>
+    <Navbar />
+    <HomeSection />
+  </React.StrictMode>
+);
+
+
